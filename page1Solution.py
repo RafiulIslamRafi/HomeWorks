905745f6fb6153ae21609b53e4530629a883d3c0
RafiulIslam <rafiulislamrafi77@gmail.com>
2020-07-03 16:22:51 +0600

    update with python as backend

---
+++ page1Solution.py
@@ -0,0 +1,4 @@
+#need to learn JSON.
+#browser e python kaj korbe, emon ekta environment dite hobe, jmon ta phpMyadmin diye PHP er ta connect kora hoice.
+#janina er jonno ki kora lagbe.
+print("i am testing it with javascript")
