01c631b21c5e29d3fd596005e023a4976f4f098e
RafiulIslam <rafiulislamrafi77@gmail.com>
2020-07-05 14:13:13 +0600

    connected with SQL Server with its Database by SQL

--- page2.php
+++ page2.php
@@ -14,42 +14,65 @@
     </style>
 </head>
 <body>
-    <a href="index.html">go to home</a>
+    <a href="index.html">go to home</a><br> <a href="page2.php">refresh</a>
     <div>
         <h3>Question 2: Write a program to insert product profile into the database and show the list into another page</h3>
         <h4>Insert your all Data about your product:</h4>
+        <h4>(kono dhoner Code, SQL : data hishebe Input na dile valo hoy.)</h4><br>
         <form action="page2.php" method="post">
+            <label>Product ID: </label>
+            <input type="text" name="id"><br><br>
+
             <label>Product Name: </label>
-            <input type="text" name="Product"><br><br>
+            <input type="text" name="name"><br><br>
 
             <label>Production Cost: </label>
-            <input type="text" name="Cost"><br><br>
+            <input type="number" name="cost"><br><br>
 
             <label>Selling price: </label>
-            <input type="text" name="sell"><br><br>
+            <input type="number" name="sell"><br><br>
 
             <input type="submit" name="submitted">
         </form>
 
         <?php
             if(isset($_REQUEST['submitted'])){
-                $txt = $_POST["Product"] . " " . $_POST["Cost"] . " " . $_POST["sell"];
-                if($txt!="  ")
-                {
-                    $txt = $txt . "\n";
-                    $myfile = fopen("testfile.txt", "a");
-                    fwrite($myfile, $txt);
-                    fclose($myfile);
-                    echo "Your Data is Stored Now"."<br>";
-                }
-                else{
-                    echo "Your Data is not Stored"."<br>";
+                $connection = mysqli_connect("127.0.0.1","root","","test"); //My Local host SQL Server.
+                //$connection = new mysqli("127.0.0.1","root","","test");
+
+                if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+                else {
+                    $query = "create table product_profile
+                    (
+                        id varchar(40),
+                        name varchar(40),
+                        cost numeric(12,2),
+                        sell numeric(12,2),
+                        primary key(id)
+                    )";
+                    if(mysqli_query($connection,$query)==true){
+                        //it will creat table if table not exist, other it doesn't do any problem.
+                    }
+
+                    $id = $_POST["id"];
+                    $name = $_POST["name"];
+                    $cost = $_POST["cost"];
+                    $sell = $_POST["sell"];
+
+                    $query = "insert into product_profile values('$id','$name','$cost','$sell')";
+
+                    //if($connection->query($query)==true){
+                    if(mysqli_query($connection,$query)==true){
+                        echo "Your Data is stored Successfully<br>";
+                    }
+                    else echo "Your Data is not Inserted BeCause, Same product ID is already stored.<br>";
+                    mysqli_close($connection);
                 }
             }
             else echo "<br>";
         ?>
 
-        <a href="page2Output.php">Show the list</a>
+        <a href="page2Output.php">Show the list of Your Stored Data</a>
     </div>
 </body>
 </html>
--- page2Output.php
+++ page2Output.php
@@ -4,23 +4,74 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Question 2 Output</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <style>
         div{
             text-align: center;
         }
+        a{
+            text-decoration: none;
+        }
+        .col-3{
+            position: relative;
+            border: black 1px solid;
+            width: 100%;
+            padding-right: 15px;
+            padding-left: 15px;
+        }
+        .row {
+            display: flex;
+            flex-wrap: wrap;
+            margin-right: 0px;
+            margin-left: 0px;
+        }
+        body {
+            font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+            display: block;
+            margin: 8px;
+        }
+
     </style>
 </head>
 <body>
+    <a href="index.html">go to home</a>
     <div>
         <?php
-            $one = 0;
-            $file = fopen("testfile.txt", "r");
-            while(!feof($file)) {
-                $one++;
-                if($one==1) echo "ProductName ProductCost SellingPrice<br>";
-                echo fgets($file) . "<br>";
+            $connection = mysqli_connect("127.0.0.1","root","","test");
+            if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+            else {
+                //echo "Connected with DataBase<br>"; 
+
+                $query = "select * from product_profile";
+                $result = mysqli_query($connection,$query);
+                
+                //$row=mysqli_fetch_array($result);
+                $first = true;
+                $len = 0;
+                while ($row=mysqli_fetch_row($result))
+                {
+                    if($first){
+                        $len = count($row);
+                        echo "<div class='container'>
+                        <div class='row'>
+                            <div class='col-3'><b>Product ID</b></div>
+                            <div class='col-3'><b>Priduct Name</b></div>
+                            <div class='col-3'><b>Production Cost</b></div>
+                            <div class='col-3'><b>Selling Cost</b></div>
+                        </div>";
+                        $first = false;
+                    }
+                    echo "<div class='row'>
+                        <div class='col-3'>$row[0]</div>
+                        <div class='col-3'>$row[1]</div>
+                        <div class='col-3'>$row[2]</div>
+                        <div class='col-3'>$row[3]</div>
+                    </div>";
+                }
+                mysqli_close($connection);
+                //echo "database connection closed";
+                if($first==false) echo "</div>";
             }
-            fclose($file);
         ?>
     </div>
 </body>
